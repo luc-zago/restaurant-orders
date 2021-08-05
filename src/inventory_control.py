0c79719a49dca98aaf75a200f6cff505d278a0f4
Lucas <luc.zago@hotmail.com>
2021-08-05 15:43:09 -0300

    Projeto finalizado

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -18,10 +18,54 @@ class InventoryControl:
     }
 
     def __init__(self):
-        pass
+        self.dishes = {
+        'hamburguer': ['pao', 'carne', 'queijo'],
+        'pizza': ['massa', 'queijo', 'molho'],
+        #'queijo-quente': ['pao', 'queijo', 'queijo'],
+        'misto-quente': ['pao', 'queijo', 'presunto'],
+        #'bauru': ['pao', 'queijo', 'presunto'],
+        'coxinha': ['massa', 'frango'],
+    }
+        self.minimum_inventory = {
+        'pao': 50,
+        'carne': 50,
+        'queijo': 100,
+        'molho': 50,
+        'presunto': 50,
+        'massa': 50,
+        'frango': 50,
+    }
+        self.inventory = {
+        'pao': 50,
+        'carne': 50,
+        'queijo': 100,
+        'molho': 50,
+        'presunto': 50,
+        'massa': 50,
+        'frango': 50,
+    }
 
     def add_new_order(self, costumer, order, day):
-        pass
+        for ingredient in self.dishes[order]:
+            if self.inventory[ingredient] > 0:
+                self.inventory[ingredient] -= 1
+            else:
+                return False
+            
 
     def get_quantities_to_buy(self):
-        pass
+        to_buy = self.minimum_inventory
+        for ingredient in self.inventory.keys():
+            to_buy[ingredient] = to_buy[ingredient] - self.inventory[ingredient]
+        return to_buy
+
+    def get_available_dishes(self):
+        available_ingredients = set()
+        dishes = set()
+        for ingredient in self.inventory:
+            if self.inventory[ingredient] > 0:
+                available_ingredients.add(ingredient)
+        for dish in self.dishes:
+            if available_ingredients > set(self.dishes[dish]):
+                dishes.add(dish)
+        return dishes
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,50 @@
 class TrackOrders:
+    def __init__(self):
+        self.track = {}
+        self.days = {}
+        self.orders = set()
+
     def __len__(self):
-        pass
+        return len(self.track)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        if costumer not in self.track:
+            self.track[costumer] = {"orders": {order: 1}, "days": set()}
+        if order not in self.track[costumer]["orders"].keys():
+            self.track[costumer]["orders"][order] = 1
+        else:
+            self.track[costumer]["orders"][order] += 1
+        if day not in self.days:
+            self.days[day] = 1
+        else:
+            self.days[day] += 1
+        self.track[costumer]["days"].add(day)
+        self.orders.add(order)
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        return [
+            order
+            for order in self.track[costumer]["orders"].keys()
+            if self.track[costumer]["orders"][order]
+            == max(self.track[costumer]["orders"].values())
+        ][0]
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        return self.orders.difference(self.track[costumer]["orders"].keys())
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        return set(self.days.keys()).difference(self.track[costumer]["days"])
 
     def get_busiest_day(self):
-        pass
+        return [
+            day
+            for day in self.days.keys()
+            if self.days[day] == max(self.days.values())
+        ][0]
 
     def get_least_busy_day(self):
-        pass
+        return [
+            day
+            for day in self.days.keys()
+            if self.days[day] == min(self.days.values())
+        ][0]
